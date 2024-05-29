e79f19741837275875998a87605d36116341c4f8
unawarexi <unawarexi@gmail.com>
2024-05-29 04:13:26 +0100

    Introduction to typescript

---
+++ typescript/intro/Intro.js
@@ -0,0 +1,5 @@
+/**
+ * to convert TS  to JS  use "tsc name of file" in the terminal
+ */
+var user = { name: "andrew", age: 40 };
+console.log(user.name);
---
+++ typescript/intro/Intro.ts
@@ -0,0 +1,7 @@
+/**
+ * to convert TS  to JS  use "tsc name of file" in the terminal
+ * this will create a js file with same name but .js extention
+ */
+
+const user = { name: "andrew", age: 40 }
+console.log(user.name)
